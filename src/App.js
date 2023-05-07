509ca0d06ff68622b543a057cb97c21ffc840ae0
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-05-07 23:55:16 +0530

    Feedback Routing

--- src/App.js
+++ src/App.js
@@ -1,48 +1,54 @@
-import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import Header from "./components/Header";
-import FeedbackItem  from "./components/FeedBackItem";
+import FeedbackItem from "./components/FeedBackItem";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import Card from "./components/shared/Card";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 
+function App({ handleDelete }) {
+  const [feedback, setFeedback] = useState(FeedbackData);
 
-function App({handleDelete}) {
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
-const [feedback, setFeedback] = useState(FeedbackData)
-
-const addFeedback = (newFeedback) => {
-  newFeedback.id = uuidv4()
-  setFeedback([newFeedback,...feedback])
-}
-
-
-const deleteFeedback = ((id) => {
-  if(window.confirm('Are you sure you want to delete?')){
-    setFeedback( feedback.filter((item) => 
-      item.id !== id
-      ))
-  }
-})
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm 
-          handleAdd={addFeedback}
-        />
-        <FeedbackStats 
-          feedback={feedback} 
-        />
-        <FeedbackList 
-          feedback={feedback}
-          handleDelete={deleteFeedback}
-        />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   );
 }
 
